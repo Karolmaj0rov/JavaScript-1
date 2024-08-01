e533638b6a9cded11f0e057f5638be49dc7f7f60
Karolmaj0rov <nurmuhammadjonazimov@gmail.com>
2024-08-01 16:52:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let name = prompt(`String typedagi malumotni kiriting`)
+
+let number = prompt('number typedagi malumotni kiriting')
+
+let lastname = prompt('boolean typedagi malumotni kiriting')
+
+
+alert(name)
+
+alert(number)
+
+alert(lastname)
+
+
+console.log(name)
+
+console.log(number)
+
+console.log(lastname)
